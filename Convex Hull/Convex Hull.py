47f2e508d0a60b5206c8e16ccb809191afa346eb
Chad <chad531206@gmail.com>
2016-05-21 14:58:14 +0800

    new window

--- Convex Hull/Convex Hull.py
+++ Convex Hull/Convex Hull.py
@@ -1,25 +1,23 @@
 import pygame
 
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE  = (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED   = (255,   0,   0)
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Convex Hull演算法作業D0240460")
+    pygame.display.set_caption("Convex Hull_D0240460")
     clock = pygame.time.Clock()
     font = pygame.font.Font("/usr/share/fonts/wenquanyi/wqy-microhei/wqy-microhei.ttc", 36)
 
-    going = True
-    while going:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                going = False
-
-        screen.fill((0, 0, 0))
-        screen.blit(font.render(str(clock.get_fps()), True, (255, 255, 255)), (10, 10))
+    while True:
+        screen.fill(WHITE)
         pygame.display.update()
 
-        clock.tick(60)
 
     pygame.quit()
 
